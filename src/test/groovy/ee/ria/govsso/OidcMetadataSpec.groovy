b979f7fa56cf976e2365176eb95241ff9891da1e
Markus Kivisalu <markus.kivisalu@nortal.com>
2021-12-05 14:54:49 +0200

    GSSO-117 Update metadata tests

--- src/test/groovy/ee/ria/govsso/OidcMetadataSpec.groovy
+++ src/test/groovy/ee/ria/govsso/OidcMetadataSpec.groovy
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*
 class OidcMetadataSpec extends GovSsoSpecification {
     Flow flow = new Flow(props)
 
-    @Feature("")
+    @Feature("OIDC_DISCOVERY_ENDPOINT")
     def "Verify discovery path"() {
         expect:
         Response response = Requests.getRequest(flow.ssoOidcService.fullConfigurationUrl)
@@ -17,44 +17,84 @@ class OidcMetadataSpec extends GovSsoSpecification {
         assertEquals((flow.ssoOidcService.baseUrl.toString() + "/"), response.getBody().jsonPath().get("issuer"), "Correct issuer")
     }
 
-   //TODO: review content
-    @Feature("")
+    //TODO: review after GSSO-161
+    @Feature("OIDC_DISCOVERY_CONTENT")
     def "Verify discovery content"() {
         expect:
         JsonPath jsonResponse = Requests.getOpenidConfiguration(flow.ssoOidcService.fullConfigurationUrl)
+
         assertEquals((flow.ssoOidcService.baseUrl.toString() + "/"), jsonResponse.get("issuer"), "Correct issuer")
+        assertEquals((flow.ssoOidcService.fullAuthenticationRequestUrl).toString(), jsonResponse.getString("authorization_endpoint"), "Correct authorization endpoint")
+        assertEquals((flow.ssoOidcService.baseUrl + "/oauth2/token").toString(), jsonResponse.getString("token_endpoint"), "Correct token endpoint")
+        assertEquals((flow.ssoOidcService.fullJwksUrl).toString(), jsonResponse.getString("jwks_uri"), "Correct jwks uri")
+
+        assertEquals(["public"], jsonResponse.getList("subject_types_supported"), "Supported subject types")
+
+        List<String> responsesSupported = jsonResponse.getList("response_types_supported")
+        def responseList = ["code", "code id_token", "id_token", "token id_token", "token", "token id_token code"]
+        responseList.each {
+            assertTrue(responsesSupported.contains(it), "Response Types Supported. Contains $it")
+        }
+
+        List<String> claimsSupported = jsonResponse.getList("claims_supported")
+        def claimsList = ["sub", "acr", "amr", "at_has", "aud", "auth_time", "exp", "iat", "iss", "jti", "nonce", "profile_attributes.date_of_birth", "profile_attributes.family_name", "profile_attributes.given_name", "sid"]
+        claimsList.each {
+            assertTrue(claimsSupported.contains(it), "Claims supported. Contains $it")
+        }
+
+        List<String> grantTypesSupported = jsonResponse.getList("grant_types_supported")
+        def grantTypesList = ["authorization_code", "implicit", "client_credentials", "refresh_token"]
+        grantTypesList.each {
+            assertTrue(grantTypesSupported.contains(it), "Claims supported. Contains $it")
+        }
+
+        List<String> responseModesSupported = jsonResponse.getList("response_modes_supported")
+        def responseModeList = ["query", "fragment"]
+        responseModeList.each {
+            assertTrue(responseModesSupported.contains(it), "Claims supported. Contains $it")
+        }
+
+        assertEquals((flow.ssoOidcService.baseUrl.toString() + "/userinfo"), jsonResponse.get("userinfo_endpoint"), "Correct userinfo endpoint")
+
         List<String> scopesSupported = jsonResponse.getList("scopes_supported")
-        def scopeList = ["openid"] //, "eidas", "eidasonly", "idcard", "mid", "smartid", "email", "phone"]
+        def scopeList = ["openid", "offline_access", "offline"]
         scopeList.each {
             assertTrue(scopesSupported.contains(it), "Scope supported. Contains $it")
         }
 
-        assertEquals("code", jsonResponse.getList("response_types_supported")[0], "Supported response types")
-        assertEquals("public", jsonResponse.getList("subject_types_supported")[0], "Supported subject types")
-        assertEquals(["sub"], jsonResponse.getList("claims_supported"), "Supported claims")
+        List<String> tokenEndpointAuthMethodsSupported = jsonResponse.getList("token_endpoint_auth_methods_supported")
+        def tokenEndpointAuthMethodsList = ["client_secret_post", "client_secret_basic", "private_key_jwt", "none"]
+        tokenEndpointAuthMethodsList.each {
+            assertTrue(tokenEndpointAuthMethodsSupported.contains(it), "Scope supported. Contains $it")
+        }
 
-        assertTrue(jsonResponse.getBoolean("request_parameter_supported").equals(true))
-        assertTrue(jsonResponse.getBoolean("request_uri_parameter_supported").equals(true))
-        assertTrue(jsonResponse.getBoolean("require_request_uri_registration").equals(true))
-        assertTrue(jsonResponse.getBoolean("claims_parameter_supported").equals(false))
-        assertTrue(jsonResponse.getBoolean("backchannel_logout_supported").equals(true))
-        assertTrue(jsonResponse.getBoolean("backchannel_logout_session_supported").equals(true))
-        assertTrue(jsonResponse.getBoolean("frontchannel_logout_supported").equals(true))
-        assertTrue(jsonResponse.getBoolean("frontchannel_logout_session_supported").equals(true))
+        List<String> userinfoSigningAlgValuesSupported = jsonResponse.getList("userinfo_signing_alg_values_supported")
+        def userinfoSigningAlgValuesList = ["none", "RS256"]
+        userinfoSigningAlgValuesList.each {
+            assertTrue(userinfoSigningAlgValuesSupported.contains(it), "Scope supported. Contains $it")
+        }
 
+        assertEquals(["RS256"], jsonResponse.getList("id_token_signing_alg_values_supported"), "Supported alg values")
 
-        assertEquals("authorization_code", jsonResponse.getList("grant_types_supported")[0], "Supported grant types")
-        assertEquals("RS256", jsonResponse.getList("id_token_signing_alg_values_supported")[0], "Supported alg values")
-
-        assertEquals((flow.ssoOidcService.baseUrl + "/oauth2/token").toString(), jsonResponse.getString("token_endpoint"), "Correct token endpoint")
-        assertEquals((flow.ssoOidcService.baseUrl + "/userinfo").toString(), jsonResponse.getString("userinfo_endpoint"), "Correct userinfo endpoint")
-        assertEquals((flow.ssoOidcService.fullAuthenticationRequestUrl).toString(), jsonResponse.getString("authorization_endpoint"), "Correct authorization endpoint")
-        assertEquals((flow.ssoOidcService.fullJwksUrl).toString(), jsonResponse.getString("jwks_uri"), "Correct jwks uri")
+        assertTrue(jsonResponse.getBoolean("request_parameter_supported") == true)
+        assertTrue(jsonResponse.getBoolean("request_uri_parameter_supported") == (true))
+        assertTrue(jsonResponse.getBoolean("require_request_uri_registration") == (true))
+        assertTrue(jsonResponse.getBoolean("claims_parameter_supported") == (false))
+        assertTrue(jsonResponse.getBoolean("backchannel_logout_supported") == (true))
+        assertTrue(jsonResponse.getBoolean("backchannel_logout_session_supported") == (true))
+        assertTrue(jsonResponse.getBoolean("frontchannel_logout_supported") == (true))
+        assertTrue(jsonResponse.getBoolean("frontchannel_logout_session_supported") == (true))
         assertEquals((flow.ssoOidcService.fullRevocationUrl).toString(), jsonResponse.getString("revocation_endpoint"), "Correct revocation endpoint")
         assertEquals((flow.ssoOidcService.fullLogoutUrl).toString(), jsonResponse.getString("end_session_endpoint"), "Correct session end endpoint")
+
+        List<String> codeChallengeMethodsSupported = jsonResponse.getList("code_challenge_methods_supported")
+        def codeChallengeMethodList = ["plain", "S256"]
+        codeChallengeMethodList.each {
+            assertTrue(codeChallengeMethodsSupported.contains(it), "Scope supported. Contains $it")
+        }
     }
 
-    @Feature("")
+    @Feature("OIDC_ENDPOINTS")
     def "Verify keystore endpoint"() {
         expect:
         JsonPath jsonResponse = Requests.getOpenidConfiguration(flow.ssoOidcService.fullConfigurationUrl)
