9e9a472870362e28c58ae04a35493eab41e25d62
Markus Kivisalu <markus.kivisalu@nortal.com>
2022-03-17 16:23:28 +0200

    GSSO-219 Update tests

--- src/test/groovy/ee/ria/govsso/OidcAuthenticationRequestSpec.groovy
+++ src/test/groovy/ee/ria/govsso/OidcAuthenticationRequestSpec.groovy
@@ -48,25 +48,25 @@ class OidcAuthenticationRequestSpec extends GovSsoSpecification {
 
     @Unroll
     @Feature("OIDC_REQUEST")
-    def "Start SSO authentication with invalid parameter: #paramKey"() {
+    def "Start OIDC authentication with invalid parameter: #paramKey"() {
         expect:
         Map<String, String> paramsMap = OpenIdUtils.getAuthorizationParametersWithDefaults(flow)
         paramsMap.put(paramKey, paramValue)
         Response response = Steps.startAuthenticationInSsoOidcWithParams(flow, paramsMap)
 
-        assertEquals(302, response.statusCode(), "Correct HTTP status code is returned")
+        assertEquals(statusCode, response.statusCode(), "Correct HTTP status code is returned")
         assertEquals(error, Utils.getParamValueFromResponseHeader(response, "error"), "Error parameter exists")
         assertEquals( errorMessage, Utils.getParamValueFromResponseHeader(response, "error_description"), "Correct error message is returned")
 
         where:
-        paramKey            | paramValue | error                       | errorMessage
-        "prompt"            | "none"     | "login_required"            | "The Authorization Server requires End-User authentication. Prompt 'none' was requested, but no existing login session was found."
-        "scope"             | "invalid"  | "invalid_scope"             | "The requested scope is invalid, unknown, or malformed. The OAuth 2.0 Client is not allowed to request scope 'invalid'."
-        "state"             | "invalid"  | "invalid_state"             | "The state is missing or does not have enough characters and is therefore considered too weak. Request parameter 'state' must be at least be 8 characters long to ensure sufficient entropy."
-        "response_type"     | "token"    | "unsupported_response_type" | "The authorization server does not support obtaining a token using this method. The client is not allowed to request response_type 'token'."
-        "response_type"     | "invalid"  | "unsupported_response_type" | "The authorization server does not support obtaining a token using this method. The client is not allowed to request response_type 'invalid'."
-        "client_id"         | "invalid"  | "invalid_client"            | "Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method). The requested OAuth 2.0 Client does not exist."
-        "redirect_uri"      | "invalid"  | "invalid_request"           | "The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed. The 'redirect_uri' parameter does not match any of the OAuth 2.0 Client's pre-registered redirect urls."
+        paramKey            | paramValue | error                       | statusCode | errorMessage
+        "prompt"            | "none"     | "login_required"            | 303        | "The Authorization Server requires End-User authentication. Prompt 'none' was requested, but no existing login session was found."
+        "scope"             | "invalid"  | "invalid_scope"             | 303        | "The requested scope is invalid, unknown, or malformed. The OAuth 2.0 Client is not allowed to request scope 'invalid'."
+        "state"             | "invalid"  | "invalid_state"             | 303        | "The state is missing or does not have enough characters and is therefore considered too weak. Request parameter 'state' must be at least be 8 characters long to ensure sufficient entropy."
+        "response_type"     | "token"    | "unsupported_response_type" | 303        | "The authorization server does not support obtaining a token using this method. The client is not allowed to request response_type 'token'."
+        "response_type"     | "invalid"  | "unsupported_response_type" | 303        | "The authorization server does not support obtaining a token using this method. The client is not allowed to request response_type 'invalid'."
+        "client_id"         | "invalid"  | "invalid_client"            | 302        | "Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method). The requested OAuth 2.0 Client does not exist."
+        "redirect_uri"      | "invalid"  | "invalid_request"           | 302        | "The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed. The 'redirect_uri' parameter does not match any of the OAuth 2.0 Client's pre-registered redirect urls."
     }
 
     @Unroll
@@ -77,17 +77,15 @@ class OidcAuthenticationRequestSpec extends GovSsoSpecification {
         paramsMap.remove(missingParam)
         Response response = Steps.startAuthenticationInSsoOidcWithParams(flow, paramsMap)
 
-        assertEquals(302, response.statusCode(), "Correct HTTP status code is returned")
+        assertEquals(statusCode, response.statusCode(), "Correct HTTP status code is returned")
         assertEquals(error, Utils.getParamValueFromResponseHeader(response, "error"), "Error parameter exists")
         assertEquals(errorMessage, Utils.getParamValueFromResponseHeader(response, "error_description"), "Correct error message is returned")
 
         where:
-        missingParam    | error                       | errorMessage
-        "state"         | "invalid_state"             | "The state is missing or does not have enough characters and is therefore considered too weak. Request parameter 'state' must be at least be 8 characters long to ensure sufficient entropy."
-        "response_type" | "unsupported_response_type" | "The authorization server does not support obtaining a token using this method. `The request is missing the 'response_type' parameter."
-        "client_id"     | "invalid_client"            | "Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method). The requested OAuth 2.0 Client does not exist."
-//        "scope"         | "invalid_scope"             | "The requested scope is invalid, unknown, or malformed. The OAuth 2.0 Client is not allowed to request scope 'invalid'."
-//        "redirect_uri"  | "invalid_request"           | "The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed. The 'redirect_uri' parameter does not match any of the OAuth 2.0 Client's pre-registered redirect urls."
+        missingParam    | error                       | statusCode | errorMessage
+        "state"         | "invalid_state"             | 303        | "The state is missing or does not have enough characters and is therefore considered too weak. Request parameter 'state' must be at least be 8 characters long to ensure sufficient entropy."
+        "response_type" | "unsupported_response_type" | 303        | "The authorization server does not support obtaining a token using this method. `The request is missing the 'response_type' parameter."
+        "client_id"     | "invalid_client"            | 302        | "Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method). The requested OAuth 2.0 Client does not exist."
     }
 
     @Feature("OIDC_LANGUAGE_SELECTION")
@@ -112,7 +110,7 @@ class OidcAuthenticationRequestSpec extends GovSsoSpecification {
         "ET"       | "Estonian with big letters"               | "Riigi autentimisteenus - Turvaline autentimine asutuste e-teenustes"
         "RU"       | "Russian with big letters"                | "Национальный сервис аутентификации - Для безопасной аутентификации в э-услугах"
         "EN"       | "English with big letters"                | "National authentication service - Secure authentication for e-services"
-         null      | "Without locale parameter"                | "Riigi autentimisteenus - Turvaline autentimine asutuste e-teenustes"
+        null       | "Without locale parameter"                | "Riigi autentimisteenus - Turvaline autentimine asutuste e-teenustes"
     }
 
     @Feature("OIDC_REQUEST")
@@ -159,17 +157,17 @@ class OidcAuthenticationRequestSpec extends GovSsoSpecification {
 
         Response oidcServiceAuthResponse = Requests.getRequestWithCookiesAndParams(flow, flow.ssoOidcService.fullAuthenticationRequestUrl, flow.ssoOidcService.cookies, queryParams, Collections.emptyMap())
 
-        assertEquals(302, oidcServiceAuthResponse.getStatusCode(), "Correct HTTP status code is returned")
+        assertEquals(statusCode, oidcServiceAuthResponse.getStatusCode(), "Correct HTTP status code is returned")
         assertEquals(error, Utils.getParamValueFromResponseHeader(oidcServiceAuthResponse,"error_description"), "Correct HTTP status code is returned")
 
         where:
-        reason                    | clientId    | loginVerifier    | redirectUri    | responseType    | scope       | state   | error
-        "Incorrect client_id"     | "scope"     | "login_verifier" | "redirect_uri" | "response_type" | "scope"     | "state" | "Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method). The requested OAuth 2.0 Client does not exist."
-        "Incorrect login_verifier"| "client_id" | "scope"          | "redirect_uri" | "response_type" | "scope"     | "state" | "The resource owner or authorization server denied the request. The login verifier has already been used, has not been granted, or is invalid."
-        "Incorrect redirect_uri"  | "client_id" | "login_verifier" | "scope"        | "response_type" | "scope"     | "state" | "The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed. The 'redirect_uri' parameter does not match any of the OAuth 2.0 Client's pre-registered redirect urls."
-        "Incorrect response_type" | "client_id" | "login_verifier" | "redirect_uri" | "scope"         | "scope"     | "state" | "The authorization server does not support obtaining a token using this method. The client is not allowed to request response_type 'openid'."
-        "Incorrect scope"         | "client_id" | "login_verifier" | "redirect_uri" | "response_type" | "client_id" | "state" | "The requested scope is invalid, unknown, or malformed. The OAuth 2.0 Client is not allowed to request scope 'client-a'."
-        "Incorrect state"         | "client_id" | "login_verifier" | "redirect_uri" | "response_type" | "scope"     | "scope" | "The state is missing or does not have enough characters and is therefore considered too weak. Request parameter 'state' must be at least be 8 characters long to ensure sufficient entropy."
+        reason                    | clientId    | loginVerifier    | redirectUri    | responseType    | scope       | state   | statusCode | error
+        "Incorrect client_id"     | "scope"     | "login_verifier" | "redirect_uri" | "response_type" | "scope"     | "state" | 302        | "Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method). The requested OAuth 2.0 Client does not exist."
+        "Incorrect login_verifier"| "client_id" | "scope"          | "redirect_uri" | "response_type" | "scope"     | "state" | 303        | "The resource owner or authorization server denied the request. The login verifier has already been used, has not been granted, or is invalid."
+        "Incorrect redirect_uri"  | "client_id" | "login_verifier" | "scope"        | "response_type" | "scope"     | "state" | 302        | "The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed. The 'redirect_uri' parameter does not match any of the OAuth 2.0 Client's pre-registered redirect urls."
+        "Incorrect response_type" | "client_id" | "login_verifier" | "redirect_uri" | "scope"         | "scope"     | "state" | 303        | "The authorization server does not support obtaining a token using this method. The client is not allowed to request response_type 'openid'."
+        "Incorrect scope"         | "client_id" | "login_verifier" | "redirect_uri" | "response_type" | "client_id" | "state" | 303        | "The requested scope is invalid, unknown, or malformed. The OAuth 2.0 Client is not allowed to request scope 'client-a'."
+        "Incorrect state"         | "client_id" | "login_verifier" | "redirect_uri" | "response_type" | "scope"     | "scope" | 303        | "The state is missing or does not have enough characters and is therefore considered too weak. Request parameter 'state' must be at least be 8 characters long to ensure sufficient entropy."
     }
 
     @Unroll
@@ -193,17 +191,17 @@ class OidcAuthenticationRequestSpec extends GovSsoSpecification {
 
         Response oidcServiceAuthResponse = Requests.getRequestWithCookiesAndParams(flow, flow.ssoOidcService.fullAuthenticationRequestUrl, flow.ssoOidcService.cookies, queryParams, Collections.emptyMap())
 
-        assertEquals(302, oidcServiceAuthResponse.getStatusCode(), "Correct HTTP status code is returned")
+        assertEquals(statusCode, oidcServiceAuthResponse.getStatusCode(), "Correct HTTP status code is returned")
         assertEquals(error, Utils.getParamValueFromResponseHeader(oidcServiceAuthResponse,"error_description"), "Correct HTTP status code is returned")
 
         where:
-        reason                       | clientId    | consentVerifier    | redirectUri    | responseType    | scope       | state   | error
-        "Incorrect client_id"        | "scope"     | "consent_verifier" | "redirect_uri" | "response_type" | "scope"     | "state" | "Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method). The requested OAuth 2.0 Client does not exist."
-        "Incorrect consent_verifier" | "client_id" | "scope"            | "redirect_uri" | "response_type" | "scope"     | "state" | "The resource owner or authorization server denied the request. The consent verifier has already been used, has not been granted, or is invalid."
-        "Incorrect redirect_uri"     | "client_id" | "consent_verifier" | "scope"        | "response_type" | "scope"     | "state" | "The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed. The 'redirect_uri' parameter does not match any of the OAuth 2.0 Client's pre-registered redirect urls."
-        "Incorrect responseType"     | "client_id" | "consent_verifier" | "redirect_uri" | "scope"         | "scope"     | "state" | "The authorization server does not support obtaining a token using this method. The client is not allowed to request response_type 'openid'."
-        "Incorrect scope"            | "client_id" | "consent_verifier" | "redirect_uri" | "response_type" | "client_id" | "state" | "The requested scope is invalid, unknown, or malformed. The OAuth 2.0 Client is not allowed to request scope 'client-a'."
-        "Incorrect state"            | "client_id" | "consent_verifier" | "redirect_uri" | "response_type" | "scope"     | "scope" | "The state is missing or does not have enough characters and is therefore considered too weak. Request parameter 'state' must be at least be 8 characters long to ensure sufficient entropy."
+        reason                       | clientId    | consentVerifier    | redirectUri    | responseType    | scope       | state   | statusCode | error
+        "Incorrect client_id"        | "scope"     | "consent_verifier" | "redirect_uri" | "response_type" | "scope"     | "state" | 302        | "Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method). The requested OAuth 2.0 Client does not exist."
+        "Incorrect consent_verifier" | "client_id" | "scope"            | "redirect_uri" | "response_type" | "scope"     | "state" | 303        | "The resource owner or authorization server denied the request. The consent verifier has already been used, has not been granted, or is invalid."
+        "Incorrect redirect_uri"     | "client_id" | "consent_verifier" | "scope"        | "response_type" | "scope"     | "state" | 302        | "The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed. The 'redirect_uri' parameter does not match any of the OAuth 2.0 Client's pre-registered redirect urls."
+        "Incorrect responseType"     | "client_id" | "consent_verifier" | "redirect_uri" | "scope"         | "scope"     | "state" | 303        | "The authorization server does not support obtaining a token using this method. The client is not allowed to request response_type 'openid'."
+        "Incorrect scope"            | "client_id" | "consent_verifier" | "redirect_uri" | "response_type" | "client_id" | "state" | 303        | "The requested scope is invalid, unknown, or malformed. The OAuth 2.0 Client is not allowed to request scope 'client-a'."
+        "Incorrect state"            | "client_id" | "consent_verifier" | "redirect_uri" | "response_type" | "scope"     | "scope" | 303        | "The state is missing or does not have enough characters and is therefore considered too weak. Request parameter 'state' must be at least be 8 characters long to ensure sufficient entropy."
     }
 
     @Feature("OIDC_REQUEST")
