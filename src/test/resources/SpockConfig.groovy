91f3ae2582418a36499f0b286588ccf57fad4f16
Markus Kivisalu <markus.kivisalu@nortal.com>
2022-03-21 14:41:54 +0200

    GSSO-304 Apply parallel test execution

--- src/test/resources/SpockConfig.groovy
+++ src/test/resources/SpockConfig.groovy
@@ -1,13 +1,12 @@
 import org.spockframework.runtime.model.parallel.ExecutionMode
-import spock.config.ParallelConfiguration
 
 runner {
     filterStackTrace false
     optimizeRunOrder true
     parallel {
-        enabled false
+        enabled true
         defaultSpecificationExecutionMode = ExecutionMode.CONCURRENT
         defaultExecutionMode = ExecutionMode.SAME_THREAD
-        dynamic(2.0)
+        fixed(4)
     }
 }
