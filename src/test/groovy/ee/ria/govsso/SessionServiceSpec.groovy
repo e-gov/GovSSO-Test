170c0c37846c74ad72a7f671faa1bf1d4cf3f20e
Alar Kvell <alar.kvell@nortal.com>
2025-08-29 13:26:41 +0300

    Revert "AUT-2148 Enable temporarily disabled tests"
    This reverts commit c280ed1d1ba724445d2d86b82b889caa3bb5caed.

--- src/test/groovy/ee/ria/govsso/SessionServiceSpec.groovy
+++ src/test/groovy/ee/ria/govsso/SessionServiceSpec.groovy
@@ -6,6 +6,7 @@ import com.nimbusds.jwt.SignedJWT
 import io.qameta.allure.Feature
 import io.restassured.filter.cookie.CookieFilter
 import io.restassured.response.Response
+import spock.lang.Ignore
 import spock.lang.Unroll
 
 import static org.hamcrest.Matchers.is
@@ -681,6 +682,7 @@ class SessionServiceSpec extends GovSsoSpecification {
         assertThat("Correct message", initLogout.jsonPath().getString("message"), is("Ebakorrektne päring."))
     }
 
+    @Ignore("AUT-2148")
     @Feature("LOGOUT_INIT_ENDPOINT")
     def "Log out request with empty post_logout_redirect_uri parameter value"() {
         expect:
@@ -697,6 +699,7 @@ class SessionServiceSpec extends GovSsoSpecification {
         assertThat("Correct message", initLogout.jsonPath().getString("message"), is("Ebakorrektne päring."))
     }
 
+    @Ignore("AUT-2148")
     @Feature("LOGOUT_INIT_ENDPOINT")
     def "Log out request with missing post_logout_redirect_uri parameter"() {
         expect:
